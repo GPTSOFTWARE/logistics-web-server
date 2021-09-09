f32da94be799849a9df3f5ec8d4e94355f6be982
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-09-09 16:55:52 +0700

    Generate relationship for SOI,SOH,Product

--- src/controllers/product/product.controller.ts
+++ src/controllers/product/product.controller.ts
@@ -4,16 +4,15 @@ import db from "../../utils/db";
 // import { getRepository } from 'typeorm';
 
 const getProducts = async (req: Request, res: Response): Promise<Response> => {
-    // const page = +req?.query?.page || 1;
-    // const page_size = +req?.query?.page_size || 10;
-    // const [data, total] = await (await db)
-    //     .getRepository(Product)
-    //     .createQueryBuilder("product")
-    //     .take(page_size)
-    //     .skip((page - 1) * page_size)
-    //     .getManyAndCount();
-    // return res.json({ total, data });
-    return res.json("Success")
+    const page = +req?.query?.page || 1;
+    const page_size = +req?.query?.page_size || 10;
+    const [data, total] = await (await db)
+        .getRepository(Product)
+        .createQueryBuilder("product")
+        .take(page_size)
+        .skip((page - 1) * page_size)
+        .getManyAndCount();
+    return res.json({ total, data });
 };
 
 
