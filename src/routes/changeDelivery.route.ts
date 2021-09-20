5c5162793d9abcc6b84bdead5cdaa1cee18d59fc
longkuroko <longsteve.jobs@gmail.com>
2021-09-20 20:47:22 +0700

    update

--- src/routes/changeDelivery.route.ts
+++ src/routes/changeDelivery.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { getDelivery, getDeliveryById } from "../controllers/deliveryOrder/delivery.controller";
 import { switchDelivery } from "../controllers/deliveryOrder/deliveryOrder.controller";
 
 const router = Router();
 
+router.get('/delivery', getDelivery);
+router.get('/delivery/:id', getDeliveryById);
 router.post('/delivery-switch', switchDelivery);
 export default router;
