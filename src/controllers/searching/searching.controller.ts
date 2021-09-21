3eb2bfb9933d8b6665deb54ce6e653494368b024
longkuroko <longsteve.jobs@gmail.com>
2021-09-21 20:37:19 +0700

    update searching

--- src/controllers/searching/searching.controller.ts
+++ src/controllers/searching/searching.controller.ts
@@ -6,30 +6,40 @@ import { SaleOrder } from "../../entity/SaleOrder";
 export const searchingOrder = async (req: Request, res: Response) =>{
 
     try{ 
-        const searching = req.query.customerPhone;
+        const phone = req.query.customerPhone;
         const name = req.query.name;
         const date = req.query.date;
-        // const parseDate = Date.parse(date.toString());
-        var date1 = new Date();
-        // var date1 = date.toISOString('dd-MM-yyyy');
-        var date2 = moment(date1).format("MMM Do YY");     
-        // console.log(date2);
+        const reqName = name.toString().toLowerCase();
+        const parseDate = Date.parse(date.toString());
+        const date2 = moment(parseDate).format("MMM Do YY");     
+        console.log(date2);
 
         const order = await getRepository(SaleOrder)
                             .createQueryBuilder('order')
-                            .select()
                             .leftJoinAndSelect('order.products', 'product')
                             .leftJoinAndSelect('product.unit', 'unit_id')
                             .leftJoinAndSelect('order.paymentMethod', 'payment')
                             .leftJoinAndSelect('order.categories', 'Category')
                             .leftJoinAndSelect('order.unit', 'unit')
-                            .where('order.customerPhone = :phone', {phone:searching})
-                            .andWhere('order.customerName = :name', {name: name})
-                            .andWhere('moment(order.createdAt).format("MMM Do YY) = :time',{time: date2 })
+                            .where('order.customerPhone = :phone', { phone: phone})
                             .getMany();
-        console.log(order);
-  
-        return res.status(200).json({message: "success"});
+        if(order){
+            let newArray = [];
+            for(let item of order){
+                const date = item.createdAt;
+                const newdate = moment(date).format("MMM Do YY");
+                const newName = item.customerName.toLowerCase();
+                
+                if(newName == reqName &&  newdate == date2){
+                    newArray.push(item);
+                }
+            }
+            console.log(newArray);
+            res.status(200).json(newArray);
+        }
+        else{
+            return res.status(404).json({message: "Not Found"});
+        }
     }
     catch(err){
         console.log(err);
