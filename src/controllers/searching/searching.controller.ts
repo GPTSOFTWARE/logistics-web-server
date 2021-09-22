b0143234697df07043dea4b12c5bbf419b9c82fe
longkuroko <longsteve.jobs@gmail.com>
2021-09-22 11:14:50 +0700

    fix searching

--- src/controllers/searching/searching.controller.ts
+++ src/controllers/searching/searching.controller.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
 import moment from "moment";
+import { Repository } from "typeorm";
 import { getRepository } from "typeorm/globals";
 import { SaleOrder } from "../../entity/SaleOrder";
 
 export const searchingOrder = async (req: Request, res: Response) =>{
 
     try{ 
-        const phone = req.body.customerPhone;
-        const name = req.body.name;
-        const date = req.body.date;
+        // const customerphone = req.query.customerPhone;
+        // const name = req.body.name;
+        // const date = req.body.date;
+        const {customerPhone , name, date} = req.body;
 
+        const phone = customerPhone.toString();
         const reqName = name.toString().toLowerCase();
         const parseDate = Date.parse(date.toString());
-        const date2 = moment(parseDate).format("MMM Do YY");     
+        const date2 = moment(parseDate).format("MMM Do YY");  
 
+        console.log(phone);
+        console.log(date2);
         const order = await getRepository(SaleOrder)
                             .createQueryBuilder('order')
                             .leftJoinAndSelect('order.products', 'product')
@@ -21,15 +26,17 @@ export const searchingOrder = async (req: Request, res: Response) =>{
                             .leftJoinAndSelect('order.paymentMethod', 'payment')
                             .leftJoinAndSelect('order.categories', 'Category')
                             .leftJoinAndSelect('order.unit', 'unit')
-                            .where('order.customerPhone = :phone', { phone: phone})
+                            .where('order.customerPhone = :phone', { phone: customerPhone})
                             .getMany();
+        console.log(order);
         if(order){
             let newArray = [];
             for(let item of order){
                 const date = item.createdAt;
                 const newdate = moment(date).format("MMM Do YY");
+                console.log(newdate);
                 const newName = item.customerName.toLowerCase();
-                
+     
                 if(newName == reqName &&  newdate == date2){
                     newArray.push(item);
                 }
