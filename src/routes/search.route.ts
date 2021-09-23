0fd2bb77cee1cce5d0e9b69ac7cce65139e15fde
longkuroko <longsteve.jobs@gmail.com>
2021-09-23 18:11:45 +0700

    update searching job

--- src/routes/search.route.ts
+++ src/routes/search.route.ts
@@ -3,7 +3,7 @@ const router = Router();
 import {searchingJob, searchingOrder} from "../controllers/searching/searching.controller";
 
 router.post('/search/order', searchingOrder);
-router.post('/search/job', searchingJob);
+router.get('/search/job', searchingJob);
 
 export default router;
 
