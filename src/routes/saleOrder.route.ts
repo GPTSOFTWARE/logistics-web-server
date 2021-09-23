7c676898e9e35e39147026978d3f0a3f04f85bc1
longkuroko <longsteve.jobs@gmail.com>
2021-09-23 21:38:13 +0700

    ưpdate

--- src/routes/saleOrder.route.ts
+++ src/routes/saleOrder.route.ts
@@ -7,7 +7,10 @@ import {
     getSaleOrder,
     updateOrder,
     restoreOrder,
-    deleteMulti
+    deleteMulti,
+    getSaleOrderByTotalPrice,
+    getOrderByStatus,
+    getOrderByPhone
 }
     from "../controllers/SaleOrder/SOH.controller"
 import auth from "../middleware/auth.middleware";
@@ -15,6 +18,9 @@ import auth from "../middleware/auth.middleware";
 router.get("/orders", getSaleOrder)
 router.get("/order/:id", getOrderById)
 router.get("/order/user/:id", getOrderByUserId);
+router.get('/survey/totalprice', getSaleOrderByTotalPrice);
+router.get('/survey/status/:id', getOrderByStatus);
+router.get('/survey/phone', getOrderByPhone);
 router.post("/order", createOrder);
 router.put("/order/:id", updateOrder);
 router.patch("/order/restore/:id", restoreOrder);
