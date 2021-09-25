2fbe999e36e904cb2ae677baf1746130f475ceac
longkuroko <longsteve.jobs@gmail.com>
2021-09-25 19:27:32 +0700

    delete delivery

--- src/routes/changeDelivery.route.ts
+++ src/routes/changeDelivery.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getDelivery, getDeliveryById, getHistoryDelivery } from "../controllers/deliveryOrder/delivery.controller";
+import { deleteDelivery, getDelivery, getDeliveryById, getHistoryDelivery, restoreDelivery } from "../controllers/deliveryOrder/delivery.controller";
 import { switchDelivery, getDeliveryOrder, getDeliveryOrderById } from "../controllers/deliveryOrder/deliveryOrder.controller";
 
 const router = Router();
@@ -10,4 +10,7 @@ router.get('/delivery', getDelivery);
 router.get('/delivery/:id', getDeliveryById);
 router.get('/delivery/history/:id', getHistoryDelivery);
 router.put('/delivery/update/:id', switchDelivery);
+router.patch("/delivery/restore/:id", restoreDelivery);
+router.delete("/delivery/delete", deleteDelivery);
+
 export default router;
