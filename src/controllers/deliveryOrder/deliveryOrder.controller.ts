852134f5f52a02de975e48fcb871422cf6c154c7
longkuroko <longsteve.jobs@gmail.com>
2021-09-26 15:04:03 +0700

    update delivery

--- src/controllers/deliveryOrder/deliveryOrder.controller.ts
+++ src/controllers/deliveryOrder/deliveryOrder.controller.ts
@@ -5,6 +5,7 @@ import { DeliveryHistory } from "../../entity/DeliveryHistory";
 import { DeliveryOrder, IDeliveryOrder } from "../../entity/DeliveryOrder";
 import { Driver } from "../../entity/Driver";
 import { Status } from "../../entity/Status";
+import { createOrder } from "../SaleOrder/SOH.controller";
 
 export const switchDelivery = async (
     req: Request<any, any, any, any>,
@@ -15,49 +16,106 @@ export const switchDelivery = async (
         console.log({ data });
 
         const findSaleOrder = await getRepository(DeliveryOrder)
-            .createQueryBuilder('delivery')
-            .where('delivery.saleOrderId = :deliId', { deliId: data.saleOrderId })
-            .andWhere('delivery.statusId = :statusId', { statusId: data.statusId })
+            .createQueryBuilder('delivery') 
+            .where('delivery.id = :id', {id: req.params.id})
             .getOne();
         const findStatus = await getRepository(Status)
             .createQueryBuilder('status')
             .andWhere('status.id = :id', { id: data.statusId })
             .getOne();
+
+        // get history delivery 
+        const historyDeli = await getRepository(DeliveryHistory)
+                                .createQueryBuilder('history')
+                                .where('history.deliveryOrderId = :id', {id : req.params.id})
+                                .getMany(); 
+        console.log(historyDeli);
         var newDate = new Date();
         var date = moment(newDate);
-        if (findSaleOrder) {
-            return res.status(409).json({ message: 'Đơn hàng đang ở trong trạng thái này' }); //  delivery exists !
-        }
-        //check driverexist ;
-        const checkDriver = await getRepository(Driver).findOne(data.driverId);
-        if (!checkDriver) {
-            res.status(404).json({ message: 'Driver Not Found' });
-        }
-        else {
-            //update status delivery
-            const updateStatusDelivery = await createQueryBuilder()
-                .update(DeliveryOrder)
-                .set({
-                    statusId: data.statusId,
-                    plannedTime: date.add(8, 'h'),
-                    typeShip: data.typeShip,
-                    driver: data.driverId,
-                })
-                .where("id = :id", { id: req.params.id })
-                .execute();
-            //after update new status, add new status of delivery into delivery history.
+        if (findSaleOrder) { // nếu tìm thấy đơn hàng
+            //check status exitst in history
+            for(let item of historyDeli){
+                if( item.status == findStatus.name){
+                    //if status exists not add to history, changing delivery order side
+                    if(findSaleOrder.statusId == 1){ //thay đổi hình thức giao hàng
+                        const updateStatusDelivery = await createQueryBuilder()
+                                                    .update(DeliveryOrder)
+                                                    .set({                           
+                                                        typeShip: data.typeShip,
+                                                    })
+                                                    .where("id = :id", { id: req.params.id })
+                                                    .execute();  
+                                                    res.status(200).json({ message: "success"});   
+            
+                    }
+                    else{
+                        if(findSaleOrder.driver == data.driverId){
+                            res.status(409).json({message: "Can not change driver"});
+                        }
+                        else{
+                            res.status(200).json({ message: "success"});
+                        }
 
-            await createQueryBuilder()
-                .insert()
-                .into(DeliveryHistory)
-                .values({
-                    deliveryOrderId: req.params.id,
-                    status: findStatus?.name
-                })
-                .execute();
-            res.status(200).json({ message: 'Cập nhật tình trạng đơn hàng thành công' });
+                    }
+                }
+                else if (item.status != findStatus.name){
+                    const updateStatusDelivery = await createQueryBuilder()
+                                            .update(DeliveryOrder)
+                                            .set({
+                                                statusId: data.statusId,
+                                                plannedTime: date.add(8, 'h'),
+                                                driver: data.driverId,
+                                            })
+                                            .where("id = :id", { id: req.params.id })
+                                            .execute();
+
+                    await createQueryBuilder()
+                                .insert()
+                                .into(DeliveryHistory)
+                                .values({
+                                    deliveryOrderId: req.params.id,
+                                    status: findStatus?.name
+                                })
+                                .execute();
+                    res.status(200).json({ message: "success"});
+                }
+            }
+
 
         }
+        else{
+            res.status(404).json({ code: "404",  message: "NOT FOUND"})
+        }
+        // //check driverexist ;
+        // const checkDriver = await getRepository(Driver).findOne(data.driverId);
+        // if (!checkDriver) {
+        //     res.status(404).json({ message: 'Driver Not Found' });
+        // }
+        // else {
+        //     //update status delivery
+        //     const updateStatusDelivery = await createQueryBuilder()
+        //         .update(DeliveryOrder)
+        //         .set({
+        //             statusId: data.statusId,
+        //             plannedTime: date.add(8, 'h'),
+        //             typeShip: data.typeShip,
+        //             driver: data.driverId,
+        //         })
+        //         .where("id = :id", { id: req.params.id })
+        //         .execute();
+        //     //after update new status, add new status of delivery into delivery history.
+
+        //     await createQueryBuilder()
+        //         .insert()
+        //         .into(DeliveryHistory)
+        //         .values({
+        //             deliveryOrderId: req.params.id,
+        //             status: findStatus?.name
+        //         })
+        //         .execute();
+        //     res.status(200).json({ message: 'Cập nhật tình trạng đơn hàng thành công' });
+
+        // }
     }
     catch (error) {
         console.log(error);
