6ea9770c299f940bfde32682a40b14be084bcf0d
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-09-27 20:09:04 +0700

    Check expired time for jwt

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -16,11 +16,29 @@ export const checkJWT = async (req: Request, res: Response, next: NextFunction) 
   const { token } = req.body;
   const { userId } = (req as any).user
   const findUser = await getRepository(Account).findOne(userId);
-  jwt.verify(token, USER_SECRET, (err, user) => {
+  jwt.verify(token, USER_SECRET, (err) => {
     if (err) {
       return res.json({ message: "JWT Invalid", user: null }).status(403)
     } else {
       res.json({ message: "JWT Valid", user: findUser }).status(200)
+    }
+    next();
+  })
+
+}
+export const checkJWTAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  const { token } = req.body;
+  const { userId } = (req as any).user
+  const findUser = await getRepository(Account).findOne(userId);
+  jwt.verify(token, USER_SECRET, (err) => {
+    if (err) {
+      return res.json({ message: "JWT Invalid", user: null }).status(403)
+    } else {
+      if (findUser.role === "user") {
+        res.json({ message: "JWT Not Acceptable", user: null }).status(406)
+      } else {
+        res.json({ message: "JWT Valid", user: findUser }).status(200)
+      }
     }
     next();
   })
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 const router = Router();
 import {
-    checkJWT
+    checkJWT,
+    checkJWTAdmin
 } from "../controllers/auth/auth.controller";
 import auth from './../middleware/auth.middleware';
 
 
 router.post("/me", auth, checkJWT);
+router.post("/admin", auth, checkJWTAdmin);
 
 
 
