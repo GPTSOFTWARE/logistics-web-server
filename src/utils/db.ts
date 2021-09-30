159246e56865e4032189fe269116b4909aa9bc0b
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-09-30 10:19:44 +0700

    fix entity to deploying

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,22 @@
 import { Connection, ConnectionOptions, createConnection } from "typeorm";
-import { Product } from "../entity/Product";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./constant";
-
+import { Product } from "../entity/Product";
+import { Category } from '../entity/Category';
+import { City } from "../entity/city";
+import { Contact } from "../entity/contact";
+import { AbstractBase } from "../entity/Base";
+import { Customer } from "../entity/Customer";
+import { DeliveryHistory } from "../entity/DeliveryHistory";
+import { DeliveryOrder } from "../entity/DeliveryOrder";
+import { District } from "../entity/district";
+import { Driver } from "../entity/Driver";
+import { Job } from "../entity/job";
+import { PaymentMethod } from "../entity/payment";
+import { RequestOrder } from "../entity/RequestOrder";
+import { SaleOrder } from "../entity/SaleOrder";
+import { Status } from "../entity/Status";
+import { Unit } from "../entity/Unit";
+import { Account } from "../entity/Users";
 export const dbConfig: ConnectionOptions = {
   type: "postgres",
   host: DB_HOST,
@@ -10,7 +25,11 @@ export const dbConfig: ConnectionOptions = {
   password: DB_PASSWORD,
   port: Number(DB_PORT) || 5432,
   ssl: { rejectUnauthorized: false },
-  entities: [__dirname + "entities/**/*.entity.ts"],
+  entities: [
+    Category, City, Contact, AbstractBase, Customer, DeliveryHistory,
+    DeliveryOrder, District, Driver, Job, PaymentMethod, Product,
+    RequestOrder, SaleOrder, Status, Unit, Account
+  ],
   migrations: ['src/migration/*.ts'],
   cli: {
     "entitiesDir": "src/entity",
